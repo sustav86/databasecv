8e8ce51f567d164fbd113ff7f5bebdfbcf3b1c97
Sustavov Anton <sustav86@gmail.com>
2017-09-14 22:39:01 +0300

    Refactoring Organization class

--- src/ua/sustav/model/Organization.java
+++ src/ua/sustav/model/Organization.java
@@ -3,5 +3,16 @@ package ua.sustav.model;
 /**
  * Created by SUSTAVOV on 14.09.2017.
  */
-public class Organization {
+public class Organization extends Link {
+    private String description;
+
+    public Organization(String name, String url, String description) {
+        super(name, url);
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
