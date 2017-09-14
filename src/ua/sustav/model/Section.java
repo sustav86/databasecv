24fcf59f0f1eba33eb06c6d061ae7987b3805863
Sustavov Anton <sustav86@gmail.com>
2017-09-14 22:37:50 +0300

    Refactoring SectionType with enum

--- src/ua/sustav/model/Section.java
+++ src/ua/sustav/model/Section.java
@@ -4,5 +4,19 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Section {
-    String type;
+    private SectionType type;
+    private String description;
+
+    public Section(SectionType type, String description) {
+        this.type = type;
+        this.description = description;
+    }
+
+    public SectionType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
