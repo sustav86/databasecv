724ed97dffcb9b6e4a6ac4f879f3157d04a87ebc
Sustavov Anton <sustav86@gmail.com>
2017-09-14 19:45:00 +0300

    Create Resume entity

---
+++ src/ua/sustav/model/Resume.java
@@ -0,0 +1,12 @@
+package ua.sustav.model;
+
+import java.util.Collection;
+
+/**
+ * Created by SUSTAVOV on 14.09.2017.
+ */
+public class Resume {
+    String fullName;
+    String[] some;
+    Collection<String> collection;
+}
