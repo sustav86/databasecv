96455ee2f18dd02df371bd601576e286eaca96f5
Sustavov Anton <sustav86@gmail.com>
2017-09-14 23:22:23 +0300

    Refactoring SectionType enum

--- src/ua/sustav/model/SectionType.java
+++ src/ua/sustav/model/SectionType.java
@@ -4,5 +4,9 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public enum SectionType {
-    ACHIEVEMENTS, QUALIFICATION, EXPIRIENCE, EDUCATION;
+    OBJECTIVE,
+    ACHIEVEMENTS,
+    QUALIFICATION,
+    EXPIRIENCE,
+    EDUCATION
 }
