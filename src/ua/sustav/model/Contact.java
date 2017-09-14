093c430faa4f2db901786a1319a5b4fbad9f8fa8
Sustavov Anton <sustav86@gmail.com>
2017-09-14 22:36:57 +0300

    Refactoring ContactType with enum

--- src/ua/sustav/model/Contact.java
+++ src/ua/sustav/model/Contact.java
@@ -4,15 +4,15 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Contact {
-    private final String type;
+    private final ContactType type;
     private final String value;
 
-    public Contact(String type, String value) {
+    public Contact(ContactType type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public String getType() {
+    public ContactType getType() {
         return type;
     }
 
