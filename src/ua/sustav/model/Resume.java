193d5cb13eae6ba8b6c8a52fe22c2c7191340a5d
Sustavov Anton <sustav86@gmail.com>
2017-09-14 20:13:52 +0300

    Fixed Resume class line properties

--- src/ua/sustav/model/Resume.java
+++ src/ua/sustav/model/Resume.java
@@ -1,9 +1,12 @@
 package ua.sustav.model;
 
+import java.util.List;
+
 /**
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Resume {
     String fullName;
     String location;
+    List<Contact> contacts;
 }
