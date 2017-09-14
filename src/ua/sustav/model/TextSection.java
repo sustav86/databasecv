db4c64e25eb039ea9ba8117a87db9d19fc2aa7d2
Sustavov Anton <sustav86@gmail.com>
2017-09-14 23:56:07 +0300

    Class TextSection extends Section

--- src/ua/sustav/model/TextSection.java
+++ src/ua/sustav/model/TextSection.java
@@ -3,7 +3,7 @@ package ua.sustav.model;
 /**
  * Created by SUSTAVOV on 14.09.2017.
  */
-public class TextSection {
+public class TextSection extends Section{
     private String title;
     private String description;
 }
