ee7ded855986ba27e4fcfbc7763ae087ade531ad
Sustavov Anton <sustav86@gmail.com>
2017-09-15 00:16:56 +0300

    Refactoring ContactType enum

--- src/ua/sustav/model/ContactType.java
+++ src/ua/sustav/model/ContactType.java
@@ -4,8 +4,18 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public enum ContactType {
-    TELEPHON,
-    SKYPE,
-    EMAIL,
-    LINKEDIN
+    TELEPHON("Телефон"),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    LINKEDIN("LinkedIn");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
