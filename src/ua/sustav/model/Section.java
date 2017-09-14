44d9c2935f33efacfc5b19e5073bc82e1503f599
Sustavov Anton <sustav86@gmail.com>
2017-09-14 23:31:51 +0300

    Add ListTextSection class, refactoring Section class

--- src/ua/sustav/model/Section.java
+++ src/ua/sustav/model/Section.java
@@ -4,19 +4,6 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Section {
-    private SectionType type;
-    private String description;
+    protected SectionType type;
 
-    public Section(SectionType type, String description) {
-        this.type = type;
-        this.description = description;
-    }
-
-    public SectionType getType() {
-        return type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
