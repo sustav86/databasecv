580e7d551f32717b15bf2b5738a9b06cd152aff2
Sustavov Anton <sustav86@gmail.com>
2017-09-14 20:02:17 +0300

    Create constructor, equals, hashCode methods for Link entity

--- src/ua/sustav/model/Link.java
+++ src/ua/sustav/model/Link.java
@@ -4,6 +4,29 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Link {
-    private String name;
-    private String url;
+    private final String name;
+    private final String url;
+
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (name != null ? !name.equals(link.name) : link.name != null) return false;
+        return url.equals(link.url);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + url.hashCode();
+        return result;
+    }
 }
