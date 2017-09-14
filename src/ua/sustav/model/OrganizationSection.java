fdf84c52d1b97f875c5c3c85667bc648cb66e2af
Sustavov Anton <sustav86@gmail.com>
2017-09-14 23:57:05 +0300

    Add OrganizationSection class

---
+++ src/ua/sustav/model/OrganizationSection.java
@@ -0,0 +1,10 @@
+package ua.sustav.model;
+
+import java.util.List;
+
+/**
+ * Created by SUSTAVOV on 14.09.2017.
+ */
+public class OrganizationSection extends Section {
+    private List<Organization> values;
+}
