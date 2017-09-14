66b21f0c7a58f4b8349a82c0ce14ab4934d08403
Sustavov Anton <sustav86@gmail.com>
2017-09-14 20:15:39 +0300

    Add constructors, set, add methods for Contact entity

--- src/ua/sustav/model/Contact.java
+++ src/ua/sustav/model/Contact.java
@@ -4,4 +4,19 @@ package ua.sustav.model;
  * Created by SUSTAVOV on 14.09.2017.
  */
 public class Contact {
+    private final String type;
+    private final String value;
+
+    public Contact(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
