727fe7f25b87cce37c237ca525cfc633079c94df
Sustavov Anton <sustav86@gmail.com>
2017-09-15 21:08:46 +0300

    Create IStorage interface

---
+++ src/ua/sustav/storage/IStorage.java
@@ -0,0 +1,24 @@
+package ua.sustav.storage;
+
+import ua.sustav.model.Resume;
+
+import java.util.Collection;
+
+/**
+ * Created by SUSTAVOV on 15.09.2017.
+ */
+public interface IStorage {
+    void clear();
+
+    void save(Resume resume);
+
+    void update(Resume resume);
+
+    Resume load(String uuid);
+
+    void delete(String uuid);
+
+    Collection<Resume> getAllSorted();
+
+    int size();
+}
