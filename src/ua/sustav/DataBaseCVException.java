76850f569bbc9783066f3ff2105ff051b668ff09
Sustavov Anton <sustav86@gmail.com>
2017-09-16 19:47:19 +0300

    Create DataBaseCVException exception

---
+++ src/ua/sustav/DataBaseCVException.java
@@ -0,0 +1,36 @@
+package ua.sustav;
+
+import ua.sustav.model.Resume;
+
+/**
+ * Created by SUSTAVOV on 16.09.2017.
+ */
+public class DataBaseCVException extends Exception {
+
+    private String uuid;
+    private Resume resume;
+
+    public DataBaseCVException(String message) {
+        super(message);
+    }
+
+    public DataBaseCVException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataBaseCVException(String message, Resume resume) {
+        super(message);
+        this.resume = resume;
+    }
+
+    public DataBaseCVException(String message, Resume resume, Throwable cause) {
+        super(message, cause);
+        this.resume = resume;
+    }
+
+    public DataBaseCVException(String message, String uuid) {
+        super(message);
+        this.uuid = uuid;
+    }
+
+}
