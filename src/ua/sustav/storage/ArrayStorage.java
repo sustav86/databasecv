cf3fc0e071e058749303523bd0a94c919a87731d
Sustavov Anton <sustav86@gmail.com>
2017-09-16 14:06:04 +0300

    Realization CRUD functionality of ArrayStorage class

--- src/ua/sustav/storage/ArrayStorage.java
+++ src/ua/sustav/storage/ArrayStorage.java
@@ -2,10 +2,12 @@ package ua.sustav.storage;
 
 import ua.sustav.model.Resume;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
- * Created by SUSTAVOV on 15.09.2017.
+ * Created by SUSTAVOV
+ * on 15.09.2017.
  */
 public class ArrayStorage implements IStorage {
     private static final int LIMIT = 100;
@@ -15,36 +17,70 @@ public class ArrayStorage implements IStorage {
 
     @Override
     public void clear() {
-
+        Arrays.fill(array, null);
+        size = 0;
     }
 
     @Override
     public void save(Resume resume) {
+        checkCapacity(size);
+        checkResume(resume);
+        array[size++] = resume;
+    }
 
+    private void checkResume(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(resume)) {
+                throw new IllegalStateException("Resume has already present");
+            }
+        }
+    }
+
+    private void checkCapacity(int size) {
+        if (size > array.length) {
+            throw new IndexOutOfBoundsException("Exceeding the capacity of the array!");
+        }
     }
 
     @Override
     public void update(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(resume)) {
+                array[i] = resume;
+                break;
+            }
+
+        }
 
     }
 
     @Override
     public Resume load(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getUuid().equals(uuid)) {
+                return array[i];
+            }
+        }
         return null;
     }
 
     @Override
     public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getUuid().equals(uuid)) {
 
+            }
+        }
     }
 
     @Override
     public Collection<Resume> getAllSorted() {
-        return null;
+        Arrays.sort(array);
+        return Arrays.asList(array);
     }
 
     @Override
     public int size() {
-        return 0;
+        return array.length;
     }
 }
