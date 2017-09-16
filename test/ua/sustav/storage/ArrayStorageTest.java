cd2b485f918b3bd10748e750d762cddb908cbb1a
Sustavov Anton <sustav86@gmail.com>
2017-09-16 12:35:21 +0300

    Create ArrayStorageTest

---
+++ test/ua/sustav/storage/ArrayStorageTest.java
@@ -0,0 +1,49 @@
+package ua.sustav.storage;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by SUSTAVOV on 16.09.2017.
+ */
+class ArrayStorageTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void load() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAllSorted() {
+    }
+
+    @Test
+    void size() {
+    }
+
+}
