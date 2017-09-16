31244f60fca50b982546881a0467e76b23593b62
Sustavov Anton <sustav86@gmail.com>
2017-09-16 13:05:39 +0300

    Create Resume class constructors

--- src/ua/sustav/model/Resume.java
+++ src/ua/sustav/model/Resume.java
@@ -1,14 +1,75 @@
 package ua.sustav.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by SUSTAVOV
  *  on 14.09.2017.
  */
 public class Resume {
-    String fullName;
-    String location;
-    List<Contact> contacts;
-    List<Section> sections;
+    private String uuid;
+    private String fullName;
+    private String location;
+    private String homePage;
+    private List<Contact> contacts = new LinkedList<>();
+    private List<Section> sections = new LinkedList<>();
+
+    public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName, location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
+        this.fullName = fullName;
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
 }
--- test/ua/sustav/storage/ArrayStorageTest.java
+++ test/ua/sustav/storage/ArrayStorageTest.java
@@ -1,8 +1,10 @@
 package ua.sustav.storage;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ua.sustav.model.Resume;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by SUSTAVOV on 16.09.2017.
  */
 class ArrayStorageTest {
+    private static ArrayStorage arrayStorage = new ArrayStorage();
+    private static Resume R1, R2, R3;
+
+    @BeforeAll
+    static void initAll() {
+
+    }
+
     @BeforeEach
     void setUp() {
     }
