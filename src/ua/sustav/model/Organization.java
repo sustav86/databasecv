aa6fe54c69cbc6cc4cad5b21bc384b640c57d797
Sustavov Anton <sustav86@gmail.com>
2017-09-18 19:12:54 +0300

    Delete OrganizationPeriod class, create inner class OrganizationPeriod into Organization class

--- src/ua/sustav/model/Organization.java
+++ src/ua/sustav/model/Organization.java
@@ -1,5 +1,6 @@
 package ua.sustav.model;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -9,5 +10,19 @@ import java.util.List;
 public class Organization {
     private List<OrganizationPeriod> periods;
     private Link link;
+
+    public class OrganizationPeriod {
+        private Date startDate;
+        private Date endDate;
+        private String position;
+        private String content;
+
+        public OrganizationPeriod(Date startDate, Date endDate, String position, String content) {
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.position = position;
+            this.content = content;
+        }
+    }
 
 }
