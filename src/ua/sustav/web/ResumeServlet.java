4e26ba0f43b0f67315c6473fd09fc7d924d4f494
Sustavov Anton <sustav86@gmail.com>
2017-09-20 23:44:02 +0300

    Web project

---
+++ src/ua/sustav/web/ResumeServlet.java
@@ -0,0 +1,25 @@
+package ua.sustav.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Created by SUSTAVOV on 20.09.2017.
+ */
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer writer = response.getWriter();
+        String name = request.getParameter("name");
+        writer.write("I'm servlet " + name);
+        writer.close();
+    }
+}
